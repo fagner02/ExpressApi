1a220453b99f2f51e0b42a831cecd48a1f5514e4
Fagner <fagnerrc12@gmail.com>
2023-04-11 11:41:00 -0300

    add cors

--- dist/index.js
+++ dist/index.js
@@ -1,12 +1,15 @@
 "use strict";
 Object.defineProperty(exports, "__esModule", { value: true });
+var http = require("http");
+var https = require("https");
 var express = require("express");
+var cors = require("cors");
 var app = express();
+app.use(cors({ origin: "*" }));
 app.get("/", function (req, res) {
     console.log("Hello World");
     res.send("Hello World");
 });
-app.listen(3000, function () {
-    console.log("Server is running on port 3000");
-});
+http.createServer(app).listen(3000, function () { });
+https.createServer(app).listen(3001, function () { });
 //# sourceMappingURL=../src/src/index.js.map
--- src/index.ts
+++ src/index.ts
@@ -1,5 +1,7 @@
+import http = require("http");
+import https = require("https");
 import express = require("express");
-const cors = require("cors");
+import cors = require("cors");
 const app = express();
 
 app.use(cors({ origin: "*" }));
@@ -9,8 +11,10 @@ app.get("/", (req, res) => {
   res.send("Hello World");
 });
 
-app.listen(3000, () => {
-  console.log("Server is running on port 3000");
-});
+http.createServer(app).listen(3000, () => {});
+https.createServer(app).listen(3001, () => {});
+// app.listen(3000, () => {
+//   console.log("Server is running on port 3000");
+// });
 
 // export default app;
