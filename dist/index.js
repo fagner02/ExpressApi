2202252a2713ad7052d74e3e0a420df27b5a4ec1
Fagner <fagnerrc12@gmail.com>
2023-04-11 10:22:52 -0300

    create project

---
+++ dist/index.js
@@ -0,0 +1,12 @@
+"use strict";
+exports.__esModule = true;
+var express = require("express");
+var app = express();
+app.get("/", function (req, res) {
+    console.log("Hello World");
+    res.send("Hello World");
+});
+app.listen(3000, function () {
+    console.log("Server is running on port 3000");
+});
+//# sourceMappingURL=../src/src/index.js.map
---
+++ src/index.ts
@@ -0,0 +1,14 @@
+import express = require("express");
+
+const app = express();
+
+app.get("/", (req, res) => {
+  console.log("Hello World");
+  res.send("Hello World");
+});
+
+app.listen(3000, () => {
+  console.log("Server is running on port 3000");
+});
+
+// export default app;
