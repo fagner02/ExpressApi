dedf5c5cc761d6c7e52cac41858342785debf7dc
Fagner <fagnerrc12@gmail.com>
2023-04-12 13:34:01 -0300

    add serverless http

--- dist/index.js
+++ dist/index.js
@@ -1,7 +1,6 @@
 "use strict";
 Object.defineProperty(exports, "__esModule", { value: true });
-var http = require("http");
-var https = require("https");
+var serverless = require("serverless-http");
 var express = require("express");
 var cors = require("cors");
 var app = express();
@@ -10,6 +9,5 @@ app.get("/", function (req, res) {
     console.log("Hello World");
     res.send("Hello World");
 });
-http.createServer(app).listen(3000, function () { });
-https.createServer(app).listen(3001, function () { });
+module.exports.handler = serverless(app);
 //# sourceMappingURL=../src/src/index.js.map
--- src/index.ts
+++ src/index.ts
@@ -1,5 +1,6 @@
-import http = require("http");
-import https = require("https");
+// import http = require("http");
+// import https = require("https");
+import serverless = require("serverless-http");
 import express = require("express");
 import cors = require("cors");
 const app = express();
@@ -11,10 +12,11 @@ app.get("/", (req, res) => {
   res.send("Hello World");
 });
 
-http.createServer(app).listen(3000, () => {});
-https.createServer(app).listen(3001, () => {});
+// http.createServer(app).listen(3000, () => {});
+// https.createServer(app).listen(3001, () => {});
 // app.listen(3000, () => {
 //   console.log("Server is running on port 3000");
 // });
 
+module.exports.handler = serverless(app);
 // export default app;
