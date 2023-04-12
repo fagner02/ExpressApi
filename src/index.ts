feae099a5bf014b5b01858623d544976e5a49dc4
Fagner <fagnerrc12@gmail.com>
2023-04-12 16:42:20 -0300

    clean up

--- src/index.ts
+++ src/index.ts
@@ -1,5 +1,3 @@
-// import http = require("http");
-// import https = require("https");
 import serverless = require("serverless-http");
 import express = require("express");
 import cors = require("cors");
@@ -14,10 +12,5 @@ router.get("/", (req, res) => {
 });
 
 app.use("/.netlify/functions/index", router);
-// http.createServer(app).listen(3000, () => {});
-// https.createServer(app).listen(3001, () => {});
-// app.listen(3000, () => {
-//   console.log("Server is running on port 3000");
-// });
+
 module.exports.handler = serverless(app, {});
-// export default app;
