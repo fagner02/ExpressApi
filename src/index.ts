e8cc653879fb588682c19e743ac2eb9bd5248561
Fagner <fagnerrc12@gmail.com>
2023-04-13 08:33:43 -0300

    add dotenv

--- src/index.ts
+++ src/index.ts
@@ -1,14 +1,17 @@
 import serverless = require("serverless-http");
 import express = require("express");
 import cors = require("cors");
+
 const app = express();
 
 app.use(cors({ origin: "*" }));
 
+require("dotenv").config();
+
 const router = express.Router();
 router.get("/", (req, res) => {
   console.log("Hello World");
-  res.status(200).send("Hello World");
+  res.status(200).send("Hello World" + process.env.TEST_VAR);
 });
 
 app.use("/.netlify/functions/index", router);
